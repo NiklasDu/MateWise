7b23f4786e3f4ea620164d4706b36578e8e7ed32
NiklasDu <n.duden@icloud.com>
2025-05-20 23:18:08 +0200

    React und Tailwind initialisiert.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl font-bold">MateWise</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
