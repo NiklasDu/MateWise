8772e96bca56d8823da985848296fcb80914e198
NiklasDu <n.duden@icloud.com>
2025-05-22 14:54:50 +0200

    Feature Section 1 eingefügt.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import CtaHome from '../components/CtaHome';
+import FeatureHowTo from '../components/FeatureHowTo';
 
 function Home() {
   return (
     <>
       <Hero />
+      <FeatureHowTo />
       <CtaHome /> 
     </>
   );
